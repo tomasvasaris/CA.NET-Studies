4d40a081463e273c471d7dafca6dcb80cb738ff6
Tomas <tomasbingelis94@gmail.com>
2022-06-06 20:17:22 +0300

    composition

--- BasicMokymai/Program.cs
+++ BasicMokymai/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.Write("išvedimas ");
+            Console.Write("vienoje ");
+            Console.Write("eilutėje.");
+            Console.WriteLine("\nNauja eilutė");
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Išvedimas " + "vienoje " + "eilutėje.");             // concatenation
+            Console.WriteLine("{0} {1} {2}", "Išvedimas", "vienoje", "eilutėje.");  // composition
+            Console.WriteLine($"{"Išvedimas"} {"vienoje"} {"eilutėje."}");          // interpolation
+
+            Console.WriteLine("----------------------------");
+
+
+            Console.ReadKey();
         }
     }
 }
