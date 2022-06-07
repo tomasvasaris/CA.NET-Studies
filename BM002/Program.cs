5639d33b57670272440b16518c1ade7015b46b69
Tomas <tomasbingelis94@gmail.com>
2022-06-07 22:01:22 +0300

    Fixed Tower

--- BM002/Program.cs
+++ BM002/Program.cs
@@ -85,20 +85,20 @@
 
             // Užduotis: Sukurti Tower of Hanoi
 
-            string towerHanoi_0 = "    |    ";
-            string towerHanoi_1 = "   #|#   ";
-            string towerHanoi_2 = "  ##|##  ";
-            string towerHanoi_3 = " ###|### ";
-            string towerHanoi_4 = "####|####";
+            string towerHanoi_0 = "       |    ";
+            string towerHanoi_1 = "      #|#   ";
+            string towerHanoi_2 = "     ##|##  ";
+            string towerHanoi_3 = "    ###|### ";
+            string towerHanoi_4 = "   ####|####";
 
             // Tower of Hanoi: 01
 
             Console.WriteLine($"Tower of Hanoi 01 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_1}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_2}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_3}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_4}   {towerHanoi_0}   {towerHanoi_0}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_1}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_2}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_3}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_4}{towerHanoi_0}{towerHanoi_0}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -107,11 +107,11 @@
             // Tower of Hanoi: 02
 
             Console.WriteLine($"Tower of Hanoi 02 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_4}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_3}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_2}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_1}   {towerHanoi_0}   {towerHanoi_0}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_4}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_3}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_2}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_1}{towerHanoi_0}{towerHanoi_0}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -120,11 +120,11 @@
             // Tower of Hanoi: 03
 
             Console.WriteLine($"Tower of Hanoi 03 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -133,11 +133,11 @@
             // Tower of Hanoi: 04
 
             Console.WriteLine($"Tower of Hanoi 04 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_4}   {towerHanoi_4}   {towerHanoi_4}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_4}{towerHanoi_4}{towerHanoi_4}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -146,11 +146,11 @@
             // Tower of Hanoi: 05
 
             Console.WriteLine($"Tower of Hanoi 05 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_0}   {towerHanoi_1}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_4}   {towerHanoi_2}   {towerHanoi_3}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_0}{towerHanoi_1}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_4}{towerHanoi_2}{towerHanoi_3}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -159,11 +159,11 @@
             // Tower of Hanoi: 06
 
             Console.WriteLine($"Tower of Hanoi 06 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_3}   {towerHanoi_1}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_4}   {towerHanoi_2}   {towerHanoi_3}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_3}{towerHanoi_1}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_4}{towerHanoi_2}{towerHanoi_3}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -172,11 +172,11 @@
             // Tower of Hanoi: 07
 
             Console.WriteLine($"Tower of Hanoi 07 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_3}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_3}   {towerHanoi_0}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_3}   {towerHanoi_0}    \n" +
-                $"4eil.   {towerHanoi_3}   {towerHanoi_3}   {towerHanoi_0}    \n" +
-                $"5eil.   {towerHanoi_4}   {towerHanoi_3}   {towerHanoi_3}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_3}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_3}{towerHanoi_0}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_3}{towerHanoi_0}\n" +
+                $"4eil.{towerHanoi_3}{towerHanoi_3}{towerHanoi_0}\n" +
+                $"5eil.{towerHanoi_4}{towerHanoi_3}{towerHanoi_3}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -185,11 +185,11 @@
             // Tower of Hanoi: 08
 
             Console.WriteLine($"Tower of Hanoi 08 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_1}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_2}    \n" +
-                $"4eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_3}    \n" +
-                $"5eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_4}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_1}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_2}\n" +
+                $"4eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_3}\n" +
+                $"5eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_4}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -197,17 +197,17 @@
 
             // Tower of Hanoi: 09
 
-            string towerHanoi_1b = "   \"|\"   ";
-            string towerHanoi_2b = "  \"\"|\"\"  ";
-            string towerHanoi_3b = " \"\"\"|\"\"\" ";
-            string towerHanoi_4b = "\"\"\"\"|\"\"\"\"";
+            string towerHanoi_1b = "      \"|\"   ";
+            string towerHanoi_2b = "     \"\"|\"\"  ";
+            string towerHanoi_3b = "    \"\"\"|\"\"\" ";
+            string towerHanoi_4b = "   \"\"\"\"|\"\"\"\"";
 
             Console.WriteLine($"Tower of Hanoi 09 \n\n" +
-                $"1eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_0}    \n" +
-                $"2eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_1b}    \n" +
-                $"3eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_2b}    \n" +
-                $"4eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_3b}    \n" +
-                $"5eil.   {towerHanoi_0}   {towerHanoi_0}   {towerHanoi_4b}    \n" +
+                $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
+                $"2eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_1b}\n" +
+                $"3eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_2b}\n" +
+                $"4eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_3b}\n" +
+                $"5eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_4b}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             Console.ReadKey();
@@ -217,14 +217,14 @@
 
             Console.WriteLine("Enter your Hanoi piece: ");
 
-            string towerHanoi_x = Console.ReadLine();
+            string towerHanoi_x = "   " + Console.ReadLine();
 
             Console.WriteLine($"Tower of Hanoi 10 \n\n" +
-                $"1eil.   {towerHanoi_x}   {towerHanoi_4}   {towerHanoi_2}    \n" +
-                $"2eil.   {towerHanoi_3}   {towerHanoi_3}   {towerHanoi_3}    \n" +
-                $"3eil.   {towerHanoi_4}   {towerHanoi_2}   {towerHanoi_4}    \n" +
-                $"4eil.   {towerHanoi_3}   {towerHanoi_3}   {towerHanoi_3}    \n" +
-                $"5eil.   {towerHanoi_2}   {towerHanoi_4}   {towerHanoi_2}    \n" +
+                $"1eil.{towerHanoi_x}{towerHanoi_4}{towerHanoi_2}\n" +
+                $"2eil.{towerHanoi_3}{towerHanoi_3}{towerHanoi_3}\n" +
+                $"3eil.{towerHanoi_4}{towerHanoi_2}{towerHanoi_4}\n" +
+                $"4eil.{towerHanoi_3}{towerHanoi_3}{towerHanoi_3}\n" +
+                $"5eil.{towerHanoi_2}{towerHanoi_4}{towerHanoi_2}\n" +
                 $"       ---1stlp-------2stlp-------3stlp---- \n\n");
 
             // Tower End
