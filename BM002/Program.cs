2b9ab63ce32ec0eb0937ec7495b16d7de173952a
Tomas <tomasbingelis94@gmail.com>
2022-06-08 18:14:33 +0300

    Fixed errors

--- BM002/Program.cs
+++ BM002/Program.cs
@@ -7,6 +7,7 @@
 
             // Užduotis: Sukurti Tower of Hanoi
 
+            // Kintamieji
             string towerHanoi_0 = "       |    ";
             string towerHanoi_1 = "      #|#   ";
             string towerHanoi_2 = "     ##|##  ";
@@ -14,6 +15,7 @@
             string towerHanoi_4 = "   ####|####";
 
             // Tower of Hanoi: 01
+            // Nupieškite Tower of Hanoi. Piešiniui naudokite kintamuosius.
 
             Console.WriteLine($"Tower of Hanoi 01 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -27,6 +29,7 @@
 
 
             // Tower of Hanoi: 02
+            // Apverskite pirmą sulpelį ir išveskite visą Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 02 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -40,6 +43,7 @@
 
 
             // Tower of Hanoi: 03
+            // Išvalykite pirmą stulpelį ir išveskite tuščią Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 03 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -53,6 +57,7 @@
 
 
             // Tower of Hanoi: 04
+            // Į kiekvieno stulpelio 5eil įdėkite po 4 dalių elementą ir išveskite Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 04 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -66,6 +71,7 @@
 
 
             // Tower of Hanoi: 05
+            // Į 1stulp 5eil įdėkite 4 dalių elementą, 2stulp 5eil - 3 dalių, 3stulp 4eil - 1 dalies, 3stulp 5eil - 2 dalių, ir išveskite Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 05 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -79,6 +85,7 @@
 
 
             // Tower of Hanoi: 06
+            // Į 1stulp 4eil įdėkite tokį pat elementą kaip yra 3stulp 4eil, ir išveskite Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 06 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -92,6 +99,7 @@
 
 
             // Tower of Hanoi: 07
+            // Į visas 2stulp eilutes įdėkite tokį pat elementą kaip yra 3stulp 5eil, ir išveskite Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 07 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_3}{towerHanoi_0}\n" +
@@ -105,6 +113,7 @@
 
 
             // Tower of Hanoi: 08
+            // Į 3stulp sudėkite teisingą piramidę. 1stulp ir 2 stulp turi likti tušti, išveskite Tower of Hanoi
 
             Console.WriteLine($"Tower of Hanoi 08 \n\n" +
                 $"1eil.{towerHanoi_0}{towerHanoi_0}{towerHanoi_0}\n" +
@@ -118,7 +127,9 @@
 
 
             // Tower of Hanoi: 09
+            // Pakeiskite visų elementų dizainą iš # į " , išveskite Tower of Hanoi
 
+            // Nauji kintamieji
             string towerHanoi_1b = "      \"|\"   ";
             string towerHanoi_2b = "     \"\"|\"\"  ";
             string towerHanoi_3b = "    \"\"\"|\"\"\" ";
@@ -136,6 +147,7 @@
 
 
             // Tower of Hanoi: 10
+            // Paprašykite naudotojo nupiešti 1 sulpelio 1 eilutę. Išveskite visą Tower of Hanoi su perpiešta pirma eilute
 
             Console.WriteLine("Enter your Hanoi piece: ");
 
