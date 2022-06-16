c72cb3a6c956390c02020b031d762f849a1694c2
Tomas <tomasbingelis94@gmail.com>
2022-06-16 18:26:07 +0300

    Task complete

--- BM006/Program.cs
+++ BM006/Program.cs
@@ -128,26 +128,26 @@
             string seg20 = Convert.ToString((vietaKm > (seg0 * 19)) && (vietaKm <= seg0 * 20)).Replace("True", "V").Replace("False", "_");
 
             // Segmentai 02
-            string xseg1  = Convert.ToString(vietaKm < seg0 * 1 ).Replace("True", "").Replace("False", "-----");
-            string xseg2  = Convert.ToString(vietaKm < seg0 * 2 ).Replace("True", "").Replace("False", "-----");
-            string xseg3  = Convert.ToString(vietaKm < seg0 * 3 ).Replace("True", "").Replace("False", "-----");
-            string xseg4  = Convert.ToString(vietaKm < seg0 * 4 ).Replace("True", "").Replace("False", "-----");
-            string xseg5  = Convert.ToString(vietaKm < seg0 * 5 ).Replace("True", "").Replace("False", "-----");
-            string xseg6  = Convert.ToString(vietaKm < seg0 * 6 ).Replace("True", "").Replace("False", "-----");
-            string xseg7  = Convert.ToString(vietaKm < seg0 * 7 ).Replace("True", "").Replace("False", "-----");
-            string xseg8  = Convert.ToString(vietaKm < seg0 * 8 ).Replace("True", "").Replace("False", "-----");
-            string xseg9  = Convert.ToString(vietaKm < seg0 * 9 ).Replace("True", "").Replace("False", "-----");
-            string xseg10 = Convert.ToString(vietaKm < seg0 * 10).Replace("True", "").Replace("False", "-----");
-            string xseg11 = Convert.ToString(vietaKm < seg0 * 11).Replace("True", "").Replace("False", "-----");
-            string xseg12 = Convert.ToString(vietaKm < seg0 * 12).Replace("True", "").Replace("False", "-----");
-            string xseg13 = Convert.ToString(vietaKm < seg0 * 13).Replace("True", "").Replace("False", "-----");
-            string xseg14 = Convert.ToString(vietaKm < seg0 * 14).Replace("True", "").Replace("False", "-----");
-            string xseg15 = Convert.ToString(vietaKm < seg0 * 15).Replace("True", "").Replace("False", "-----");
-            string xseg16 = Convert.ToString(vietaKm < seg0 * 16).Replace("True", "").Replace("False", "-----");
-            string xseg17 = Convert.ToString(vietaKm < seg0 * 17).Replace("True", "").Replace("False", "-----");
-            string xseg18 = Convert.ToString(vietaKm < seg0 * 18).Replace("True", "").Replace("False", "-----");
-            string xseg19 = Convert.ToString(vietaKm < seg0 * 19).Replace("True", "").Replace("False", "-----");
-            string xseg20 = Convert.ToString(vietaKm < seg0 * 20).Replace("True", "").Replace("False", "-----");
+            string xseg1  = (vietaKm <= seg0 * 1 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg2  = (vietaKm <= seg0 * 2 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg3  = (vietaKm <= seg0 * 3 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg4  = (vietaKm <= seg0 * 4 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg5  = (vietaKm <= seg0 * 5 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg6  = (vietaKm <= seg0 * 6 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg7  = (vietaKm <= seg0 * 7 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg8  = (vietaKm <= seg0 * 8 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg9  = (vietaKm <= seg0 * 9 ).ToString().Replace("True", "").Replace("False", "------");
+            string xseg10 = (vietaKm <= seg0 * 10).ToString().Replace("True", "").Replace("False", "------");
+            string xseg11 = (vietaKm <= seg0 * 11).ToString().Replace("True", "").Replace("False", "------");
+            string xseg12 = (vietaKm <= seg0 * 12).ToString().Replace("True", "").Replace("False", "------");
+            string xseg13 = (vietaKm <= seg0 * 13).ToString().Replace("True", "").Replace("False", "------");
+            string xseg14 = (vietaKm <= seg0 * 14).ToString().Replace("True", "").Replace("False", "------");
+            string xseg15 = (vietaKm <= seg0 * 15).ToString().Replace("True", "").Replace("False", "------");
+            string xseg16 = (vietaKm <= seg0 * 16).ToString().Replace("True", "").Replace("False", "------");
+            string xseg17 = (vietaKm <= seg0 * 17).ToString().Replace("True", "").Replace("False", "------");
+            string xseg18 = (vietaKm <= seg0 * 18).ToString().Replace("True", "").Replace("False", "------");
+            string xseg19 = (vietaKm <= seg0 * 19).ToString().Replace("True", "").Replace("False", "------");
+            string xseg20 = (vietaKm <= seg0 * 20).ToString().Replace("True", "").Replace("False", "------");
 
             // Kelelis
             Console.WriteLine($"\nKelelis:");
@@ -157,22 +157,13 @@
             Console.WriteLine($"_A__{seg1}__|__{seg2}__|__{seg3}__|__{seg4}__|__{seg5}__|__{seg6}__|__{seg7}__|__{seg8}__|__{seg9}__|__{seg10}__|__{seg11}__|__{seg12}__|__{seg13}__|__{seg14}__|__{seg15}__|__{seg16}__|__{seg17}__|__{seg18}__|__{seg19}__|__{seg20}__B_");
             Console.WriteLine($" |{xseg1}{xseg2}{xseg3}{xseg4}{xseg5}{xseg6}{xseg7}{xseg8}{xseg9}{xseg10}{xseg11}{xseg12}{xseg13}{xseg14}{xseg15}{xseg16}{xseg17}{xseg18}{xseg19}{xseg20}--| ({vietaKm} km)");
             Console.WriteLine("");
-            Console.WriteLine($" |-------------------------------------------- {atstumas, 6} km ----------------------------------------------------------------| ");
-            Console.WriteLine($" |>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {laikasMinA, 5} min >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>| ");
-            Console.WriteLine($" |<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< {laikasMinB, 5} min <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<| ");
+            Console.WriteLine($" |------------------------------------------------------ {atstumas, 6} km ------------------------------------------------------| ");
+            Console.WriteLine($" |>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> {laikasMinA, 5} min >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>| ");
+            Console.WriteLine($" |<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< {laikasMinB, 5} min <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<| ");
             Console.WriteLine("");
 
             Console.WriteLine("Kelio pabaiga.\n\n");
-            // Užduočių pabaiga
-
-            /*
-            
-            Trūkumai:
-            1) >>>> ir <<<< rodyklės nesimetriškos, pradėk nuo to (ez fix (manau (tikuos)))
-            2) susidūrimo vieta ir laikas turėtgų būti int (kad nebūtų stuff'o po kablelių)
-            3) V ir -| neusisiduria. Kodėl? Vienas arba neteisingi (palyginti su pavyzdžiu)
-
-            */
+            // Komentaras: Įdomi užduotis! Iš pradžių maniau kad bus neįmanoma apsieit be IF'o, bet va
 
         }
     }
