cd31c3262e4fac98c0085342434c20f32cc17c7d
Tomas <tomasbingelis94@gmail.com>
2022-07-12 22:00:17 +0300

    StringBuilder

--- BM015/Program.cs
+++ BM015/Program.cs
@@ -6,7 +6,87 @@
         {
             //ForLoop();
             //BreakSkip();
-            ForNesting();
+            //ForNesting();
+
+            /*
+            UŽDUOTIS 1. [12-07-2022]
+            Sukurti metodą ReadIntNumber,
+            kuris paprašo vartotojo įvesti sveikąjį skaičių ir tą skaičių grąžina.
+            Jeigu vartotojas įveda blogą skaičių, tai programa turi informuoti, kad
+            įvestas blogas skaičius ir prašyti įvesti vėl. Kol vartotojas
+            neįveda tinkamo skaičiaus metodas turi vis prašyti įvesti.
+            (Hint) -> Panaudoti int.TryParse metodą ir while ciklą.
+            */
+
+            //ReadIntNumber();
+
+
+            // UŽDUOTIS 2. Konvertuoti skaičių į dvejetainį formatą
+            //Console.WriteLine("Write a number you want to convert to binary:");
+            //Console.WriteLine(ConvertToBinary(Console.ReadLine()));
+
+            // UŽDUOTIS 3. Pakelti laipsniu
+            MultiplyByDegree(3, 3);
+
+
+        }
+
+        private static void MultiplyByDegree(int number, int degree)
+        {
+            int total = number;
+
+            if (number == 0 && degree > 0) { total = 1; } else
+            {
+                degree--;
+
+                while (degree > 0)
+                {
+                    total *= number;
+                    degree--;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+
+        private static string ConvertToBinary(string input)
+        {
+            string binary = "";
+            int intInput;
+
+            if(!Int32.TryParse(input, out _)) { return "error"; } else
+            {
+                intInput = Convert.ToInt32(input);
+
+                while (intInput > 0)
+                {
+                    binary = intInput % 2 + binary;
+                    intInput /= 2;
+                }
+
+                return binary;
+            }
+        }
+
+        private static void ReadIntNumber()
+        {
+            Console.WriteLine("Please write an int number");
+            string? input = Console.ReadLine();
+            bool isInt = false;
+
+            while (!isInt)
+            {
+                if (Int32.TryParse(input, out _) == false)
+                {
+                    Console.WriteLine("That is not an int, try again");
+                    input = Console.ReadLine();
+
+                } else
+                {
+                    isInt = true;
+                    Console.WriteLine(input);
+                }
+            }
         }
 
         static void ForLoop()
---
+++ BM016/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+
+namespace BM016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            StringBuilder sb0 = new StringBuilder();            // no text
+            StringBuilder sb1 = new StringBuilder("'ello");     // with text
+
+            sb0.Append("Labas");
+            sb0.Append(", pasauli");
+            sb0.AppendLine();
+            sb0.AppendLine("What's up?");
+
+            Console.WriteLine(sb0.ToString());
+
+            sb0.Insert(6, " nuostabus");
+            Console.WriteLine(sb0.ToString());
+
+            sb0.Remove(7, 3);
+            Console.WriteLine(sb0.ToString());
+            */
+
+            Console.WriteLine(NumberTriangle());
+
+        }
+
+        static string NumberTriangle()
+        {
+            StringBuilder output = new StringBuilder();
+            int input = IntAsk();
+            Console.WriteLine();
+
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    output.Append(i+1);
+                }
+                output.AppendLine();
+            }
+
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = 0; j < input-i; j++)
+                {
+                    output.Append(input-i);
+                }
+                output.AppendLine();
+            }
+
+            return output.ToString();
+        }
+
+
+
+        static int IntAsk()
+        {
+            Console.WriteLine("Please write an int number: ");
+            string? input = Console.ReadLine();
+
+            for (;;)
+            {
+                if (Int32.TryParse(input, out _) == false)
+                {
+                    Console.WriteLine("That is not an int, try again");
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    return Convert.ToInt32(input);
+                }
+            }
+        }
+    }
+}
